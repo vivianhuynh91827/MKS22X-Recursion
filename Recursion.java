623c42357a6da2151fa298af8f5fcb78f486ab33
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-02-03 10:41:09 -0500

    fib method added and tested

--- Recursion.java
+++ Recursion.java
@@ -1,26 +1,59 @@
 public class Recursion {
 
-/**
- *@param n any non-negative value you want to take the sqrt of
- *@return the approximate sqrt of n within a tolerance of 0.001%
- */
- public static double sqrt(double n){
-   return guessSqrt(n, 1);
+ /*Recursively find the sqrt using Newton's approximation
+  *tolerance is the allowed percent error the squared answer is away from n.
+  *precondition: n is non-negative
+  */
+ public static double sqrt(double n, double tolerance){
+   return guessSqrt(n, 1, tolerance);
  }
 
- private static double guessSqrt(double n, double guess) {
+ private static double guessSqrt(double n, double guess, double tolerance) {
    double actualN = guess*guess;
    double percentError = Math.abs((actualN-n)/n);
-   if (percentError > 0.00001) {
+   if (percentError > tolerance) {
      double newGuess = ((n / guess) + guess) / 2;
-     return guessSqrt(n, newGuess);
+     return guessSqrt(n, newGuess, tolerance);
    }
    return guess;
  }
 
+ //----------------------------------------------------------------------------
+
+ /*Recursively find the n'th fibbonaci number in linear time
+  *fib(0) = 0; fib(1) = 1; fib(5) = 5
+  *precondition: n is non-negative
+  */
+ public static int fib(int n) {
+   if (n == 0) {
+     return 0;
+   }
+   if (n==1 || n==2) {
+     return 1;
+   }
+   return fibH(n, 3, 1, 1);
+ }
+
+ private static int fibH(int n, int curN, int prev1, int prev2){
+   if (curN == n) {
+     return prev1 + prev2;
+   }
+   return fibH(n, curN+1, prev2, prev1+prev2);
+ }
+
+ //----------------------------------------------------------------------------
+
  public static void main(String[] args) {
-   System.out.println(sqrt(100));
-   System.out.println(sqrt(2));
-   System.out.println(sqrt(81));
+   System.out.println(sqrt(100,0.0000001));
+   System.out.println(sqrt(2,0.0000001));
+   System.out.println(sqrt(81,0.0000001));
+   System.out.println(fib(0));
+   System.out.println(fib(1));
+   System.out.println(fib(2));
+   System.out.println(fib(3));
+   System.out.println(fib(4));
+   System.out.println(fib(5));
+   System.out.println(fib(6));
+   System.out.println("hi");
  }
 }
